76d77b7efd34292469435f11b77c700fff5e2e97
phmi <phmi.mail@gmail.com>
2013-04-25 09:58:57 +0600

    Fixed change password image size

--- PHmiClient/Properties/GlobalAssemblyInfo.cs
+++ PHmiClient/Properties/GlobalAssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.6807.55")]
-[assembly: AssemblyFileVersion("2.0.6807.55")]
+[assembly: AssemblyVersion("2.0.6809.56")]
+[assembly: AssemblyFileVersion("2.0.6809.56")]
